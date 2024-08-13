fd3fe79c531caf5f8beb0c5d003c98049ac5f6a9
Huk <minimickey427@yahoo.com>
2024-08-13 10:29:37 -0700

    added a rectangle

--- main.py
+++ main.py
@@ -1,32 +1,27 @@
 import pygame
-import random
-import sys
 
 # Initialize Pygame
 pygame.init()
 
 # Define colors
-background_colour = (0, 0, 0)
-circle_colour = (255, 255, 255)
-second_circle_colour = (255, 255, 255)
+background_colour = (255, 255, 255)
+circle_colour = (0, 0, 0)
+second_circle_colour = (0, 0, 0)
+rect_colour = (0, 0, 0)
 
 black = (0, 0, 0)
 white = (255, 255, 255)
 
 # Set up the screen
-screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("Leaves Alpha 0.0.1")
+screen = pygame.display.set_mode((1920, 1080))
+pygame.display.set_caption("Leaves Alpha 0.0.2")
 
 # Load and set the font
 font = pygame.font.Font("minecraft_font.ttf", 32)
 
-text = font.render("Leaves Alpha 0.0.1", False, white)
+text = font.render("Leaves Alpha 0.0.2", False, black)
 
 textRect = text.get_rect()
-
-#list for circles
-circles = []
-
 
 # Set the center of the rectangular object.
 textRect.center = (180, 20)
@@ -34,8 +29,8 @@ textRect.center = (180, 20)
 # Set the clock for controlling the frame rate
 fps = pygame.time.Clock()
 
-circle_speed = 3
-second_circle_speed = 3
+circle_speed = 5
+second_circle_speed = 5
 
 running = True
 
@@ -47,6 +42,11 @@ second_circle_center_x = 700
 second_circle_center_y = 600
 second_circle_radius = 30
 
+rect_center_x = 750
+rect_center_y = 800
+rect_height = 20
+rect_width = 30
+rect_speed = 5
 
 while running:
     for event in pygame.event.get():
@@ -67,13 +67,13 @@ while running:
         circle_center_x += circle_speed
 
     # Update second circle position based on arrow keys
-    if keys[pygame.K_UP]:
+    if keys[pygame.K_i]:
         second_circle_center_y -= second_circle_speed
-    if keys[pygame.K_DOWN]:
+    if keys[pygame.K_k]:
         second_circle_center_y += second_circle_speed
-    if keys[pygame.K_LEFT]:
+    if keys[pygame.K_j]:
         second_circle_center_x -= second_circle_speed
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_l]:
         second_circle_center_x += second_circle_speed
 
     # Fill the screen with the background color
@@ -83,6 +83,7 @@ while running:
     pygame.draw.circle(screen, circle_colour, (circle_center_x, circle_center_y), circle_radius)
     pygame.draw.circle(screen, second_circle_colour, (second_circle_center_x, second_circle_center_y),
                        second_circle_radius)
+    pygame.draw.rect(screen, rect_colour, (rect_center_x, rect_center_y, rect_height, rect_width))
 
     # Blit the text last to ensure it appears on top
     screen.blit(text, textRect)
